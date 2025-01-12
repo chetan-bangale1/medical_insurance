d968c083cb1d957d2958c083660f21a4398caf8c
Chetan Bangale <chetanbangale@vctcpune.com>
2025-01-12 16:32:36 +0530

    Updated

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 # JSON_FILE_PATH = r'project_data.json'
 
 PORT_NUMBER = 5005
-MODEL_FILE_PATH = r'artifacts\Linear_Model.pkl'
-JSON_FILE_PATH = r'artifacts\project_data.json'
+MODEL_FILE_PATH = r'artifacts/Linear_Model.pkl'
+JSON_FILE_PATH = r'artifacts/project_data.json'
