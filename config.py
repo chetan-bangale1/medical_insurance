a726e5bb08c183e0d2d925f0d5cdf0fe2807d079
Chetan Bangale <chetanbangale@vctcpune.com>
2025-01-12 14:44:54 +0530

    Projct Files Added

---
+++ config.py
@@ -0,0 +1,6 @@
+# MODEL_FILE_PATH = r'Linear_Model.pkl'
+# JSON_FILE_PATH = r'project_data.json'
+
+PORT_NUMBER = 5005
+MODEL_FILE_PATH = r'artifacts\Linear_Model.pkl'
+JSON_FILE_PATH = r'artifacts\project_data.json'
